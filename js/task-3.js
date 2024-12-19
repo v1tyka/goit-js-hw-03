9e91462a641f764b5234831a4f173c75c2e3afbe
v1tyka <racinvictor@gmail.com>
2024-12-19 20:30:23 +0100

    .

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function filterArray(numbers, value) {
-  const filteredNumbers = [];
+  const result = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+      result.push(numbers[i]);
     }
   }
-  return filteredNumbers;
+  return result;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
